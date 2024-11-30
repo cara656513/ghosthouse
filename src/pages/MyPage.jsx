92e41d04586648d7e9ac6d0a4ac4030d307adf11
wooseok <youkn0wthat@naver.com>
2024-12-01 05:00:06 +0900

    -[]Feat : mypage 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -6,9 +6,12 @@ const OpenModalBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100px;
-  width: 100px;
-  background-color: green;
+  height: 20px;
+  width: 70px;
+  background-color: red;
+  font-size: 10px;
+  border-radius: 10px;
+  margin-left: 200px;
 `;
 
 const OverlayModal = styled.div`
@@ -17,6 +20,7 @@ const OverlayModal = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  /* background-color: green; */
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
@@ -35,6 +39,43 @@ const ModalContent = styled.div`
   z-index: 10;
   text-align: center;
   box-sizing: border-box;
+`;
+const MyPageListProfileImg = styled.img`
+  display: block;
+  height: 150px;
+  width: 150px;
+  border-radius: 50%;
+  border: 1px solid black;
+  background-color: white;
+  position: relative; /* ::after 사용을 위해 추가 */
+
+  transition: transform 0.3s ease, box-shadow 0.3s ease, opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0; /* 마우스 호버 시 기본 이미지 숨기기 */
+  }
+
+  &::after {
+    content: '';
+    background-color: black;
+    position: absolute;
+    top: 50;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: url(./ghostProfileImg.png) no-repeat center/cover; /* 대체 이미지 설정 */
+    transform: translate(-70%, -70%) scale(1);
+    opacity: 1; /* 항상 표시 */
+    z-index: 1; /* 기존 이미지 위에 나타남 */
+    transition: transform 0.3s ease, opacity 0.3s ease;
+    text-indent: -9999em;
+  }
+
+  &:hover::after {
+    transform: translate(-50%, -50%) scale(4.5); /* 호버 시 이미지 확대 */
+    opacity: 1;
+  }
 `;
 
 const CloseModalBtn = styled.button`
@@ -194,8 +235,17 @@ const MyPage = () => {
 
   return (
     <div>
-      <h1>마이 페이지</h1>
-      <OpenModalBtn onClick={openModal}>프로필 수정</OpenModalBtn>
+      <MyPofileTeble>
+        <ProfileImageWrap>
+          <MyPageListProfileImg src={profileImg} alt="Profile" />
+        </ProfileImageWrap>
+        <ul>
+          <p>UserNickName</p>
+          <p>게시글 수</p>
+        </ul>
+        <OpenModalBtn onClick={openModal}>프로필 수정</OpenModalBtn>
+      </MyPofileTeble>
+      {/* // 프로필 수정하기 모달 */}
       {isModalOpen && (
         <OverlayModal onClick={closeModal}>
           <ModalContent onClick={(e) => e.stopPropagation()}>
@@ -209,18 +259,36 @@ const MyPage = () => {
           </ModalContent>
         </OverlayModal>
       )}
-      <ul>
+      {/* //
+      //
+      // 마이페이지 내 게시물 리스트
+      //
+      // 
+      // */}
+      <MypostList>
         {contents.map((item) => (
-          <li key={item.id}>
+          <PostCard key={item.id}>
             <p>{item.content}</p>
             <img src={item.image_url} alt={item.content || 'Image'} />
-          </li>
+          </PostCard>
         ))}
-      </ul>
-      {error && <p>Error: {error}</p>}
+        {error && <p>Error: {error}</p>}
+      </MypostList>
     </div>
   );
 };
+
+const PostCard = styled.li`
+  height: 200px;
+`;
+
+const MypostList = styled.ul`
+  height: 100px;
+  width: 300px;
+  flex-wrap: wrap;
+  gap: 100px;
+  background-color: gray;
+`;
 
 const ProfileContainer = styled.div`
   display: flex;
@@ -230,6 +298,26 @@ const ProfileContainer = styled.div`
   width: 30%;
   border: 1px solid red;
   position: relative;
+`;
+
+const ProfileImageWrap = styled.div`
+  background: none;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background-color: white;
+  background: url(./ghostProfileImg.png) no-repeat center/cover;
+  text-indent: -9999em;
+`;
+
+const MyPofileTeble = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  background-color: #444444;
+  width: 700px;
+  margin-top: 50px;
+  margin-left: 300px;
 `;
 
 export default MyPage;
