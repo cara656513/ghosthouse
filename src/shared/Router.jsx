3239d934e0b20a3b218429684ce39f6c1312371d
kyh3260 <ppp3260@naver.com>
2024-12-01 10:33:31 +0900

    update/회원가입 기능 구현 완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -8,24 +8,24 @@ import DetailPage from '../pages/DetailPage';
 import ProfilePage from '../pages/ProfilePage';
 import UpdatePostPage from '../pages/UpdatePostPage';
 import ProtectedRoute from '../components/ProtectedRoute';
-import Layout from '../components/Layout';
+// import Layout from '../components/Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/newpost" element={<NewPostPage />} />
-            <Route path="/detail" element={<DetailPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/updatepost" element={<UpdatePostPage />} />
-          </Route>
+        {/* <Route element={<Layout />}> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/newpost" element={<NewPostPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/updatepost" element={<UpdatePostPage />} />
         </Route>
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
