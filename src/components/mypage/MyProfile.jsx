5c2e2c290b16e08a77d1b194fd2aa8269925fc00
wooseok <youkn0wthat@naver.com>
2024-12-03 07:05:19 +0900

    🩹 fix : profile 오타 수정 👿

---
+++ src/components/mypage/MyProfile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  MyPofileTable,
+  ProfileImageWrap,
+  MyPageListProfileImg,
+  MyNickname,
+  OpenModalBtn
+} from '../components/mypage/myprofilestyle';
+
+const Myf = ({ profileImg, nickname, postCount, setIsModalOpen }) => {
+  return (
+    <MyPofileTable>
+      <ProfileImageWrap>
+        <MyPageListProfileImg src={profileImg || '/default-profile.png'} alt="Profile" />
+      </ProfileImageWrap>
+      <ul>
+        <p>{nickname}님 안녕하세요.</p>
+        <p>{postCount}개의 게시물이 있습니다.</p>
+      </ul>
+      <OpenModalBtn onClick={() => setIsModalOpen(true)}>프로필 수정</OpenModalBtn>
+    </MyPofileTable>
+  );
+};
+
+export default myfropile;
