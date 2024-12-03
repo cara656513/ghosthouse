7e7b9afdf1277338d11569ae599365fe0058ee20
Hobin-joo <wnghqls12@naver.com>
2024-12-03 10:25:54 +0900

    fix:home 파일복구

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,31 @@
 import { HomeForm, HomeH1, HomeWrap } from '../home/homeStyle';
 import { MdManageSearch } from 'react-icons/md';
 import hand from '../../assets/hand.png';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = ({ onScroll }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const nav = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    nav(`/search?q=${searchValue}`);
+  };
   return (
     <HomeWrap>
       <div>
         <HomeH1>WARNING</HomeH1>
       </div>
       <div>
-        <HomeForm>
-          <input type="text" placeholder="SEARCH GHOST HOUSE" />
+        <HomeForm onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="SEARCH GHOST HOUSE"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
           <button>
             <MdManageSearch />
           </button>
