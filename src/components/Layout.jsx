26f0095dfff173881cd981b82828f274e3db4ea4
Hobin-joo <wnghqls12@naver.com>
2024-12-04 21:53:07 +0900

    updated: 삭제버튼추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,25 @@
-
-
 import { Outlet } from 'react-router-dom';
 import Header from './header/Header';
-
-
+import { useUserStore } from '../zustand/userStore';
+import { useEffect } from 'react';
+import supabase from '../utils/supabaseClient';
 
 const Layout = () => {
+  const { setUser } = useUserStore();
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser();
+      setUser({ user });
+    };
+    initializeAuth();
+  }, [setUser]);
   return (
     <div>
-
-
       <Header />
       <Outlet />
-
-
     </div>
   );
 };
